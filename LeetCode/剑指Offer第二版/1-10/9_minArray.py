95c32486e4dc416e93e18faa962eb71cd976e207
tu15007959375 <1804694379@qq.com>
2022-09-12 15:20:14 +0800

    9-10 finish

---
+++ LeetCode/剑指Offer第二版/1-10/9_minArray.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def minArray(self, numbers: List[int]) -> int:
+        low, high = 0, len(numbers) - 1
+        while low < high:
+            pivot = low + (high - low) // 2
+            if numbers[pivot] < numbers[high]:
+                high = pivot
+            elif numbers[pivot] > numbers[high]:
+                low = pivot + 1
+            else:
+                high -= 1
+        return numbers[low]
+
+
+if __name__ == '__main__':
+    sl = Solution()
+    print(sl.minArray([1, 3, 5]))
